1f7e21a2d39d9ed0ab1761f5e57ae86bc1c65f03
JaiminVadadoriya <vadadoriyajaimin@gmail.com>
2024-03-06 07:51:00 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
-import { express } from "express";
+import express from "express";
 
 const app = express();
 
@@ -14,5 +15,16 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes
+
+import userRouter from "./routes/user.routes.js";
+
+
+// routes declaration
+
+const apiVersion = "v1";
+
+app.use(`/api/${apiVersion}/users`, userRouter);
 
 export { app };
